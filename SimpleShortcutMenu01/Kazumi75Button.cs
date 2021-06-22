4b288b5b490f3d36d249687a9aa894abdef42550
check5004 <check50045366@outlook.jp>
2021-06-22 17:04:57 +0900

    [サンプル]動的にイベントのあるボタンを追加

---
+++ SimpleShortcutMenu01/Kazumi75Button.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleShortcutMenu01 {
+    // オリジナルのボタンを定義する(WindowsフォームのButtonを継承)
+    public class Kazumi75Button : Button {
+        // ボタンを押して表示される文言テキスト
+        public string buttonMsg { get; set; }
+
+        // リストボックスを参照させる
+        public ListBox targetLbox { get; set; }
+
+        // ボタンへのイベントをセットする
+        public void eventMaking () {
+            this.Click += new EventHandler ( doClickEvent );
+        }
+
+        // ボタンへのイベントを解除する
+        public void eventSuspend () {
+            this.Click -= new EventHandler ( doClickEvent );
+        }
+
+        // クリックイベントの実体(参照するリストボックスに文言テキストを追加)
+        public void doClickEvent ( object sender, EventArgs e ) {
+            this.targetLbox.Items.Add ( this.buttonMsg );
+        }
+    }
+}
