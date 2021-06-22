3b00f54ff26ce83cd0bbf9f3c8a1ce1429f53d74
BuildTools <unconfigured@null.spigotmc.org>
2021-06-22 23:45:52 +0900

    MainMenuLabel.cs 作成

--- SimpleShortcutMenu01/FormMainMenu.Designer.cs
+++ SimpleShortcutMenu01/FormMainMenu.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FormMainMenu";
             this.Text = "FormMainMenu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
